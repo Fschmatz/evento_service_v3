1fe8af3329f82b66dacd2c80765f02d525f23ddf
Fschmatz <fschmatz@hotmail.com>
2021-11-27 17:05:10 -0300

    update 1.1.0
    Serviço Email

--- src/main/java/com/fschmatz/evento_service_v3/controller/InscricaoController.java
+++ src/main/java/com/fschmatz/evento_service_v3/controller/InscricaoController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.transaction.Transactional;
@@ -30,7 +31,7 @@ public class InscricaoController {
 
     //http://localhost:9092/evento/inscricao/listarInscricoes
     @RequestMapping("/listarInscricoes")
-    public ModelAndView listarInscricoes(){
+    public ModelAndView listarInscricoes() {
         ModelAndView mv = new ModelAndView("listarTodasInscricoes");
         Iterable<Inscricao> inscricaos = inscricaoRepository.findAll();
         mv.addObject("inscricaos", inscricaos);
@@ -39,31 +40,30 @@ public class InscricaoController {
 
     //http://localhost:9092/inscricao/listarInscricoesUsuario/1
     @RequestMapping("/listarInscricoesUsuario/{id}")
-    public ModelAndView listarInscricoesUsuario(@PathVariable("id") Integer id){
+    public ModelAndView listarInscricoesUsuario(@PathVariable("id") Integer id) {
 
         //SOMENTE com checkin = 0
         Usuario usuarioSite = new Usuario();
         usuarioSite.setId_usuario(id);
         ModelAndView mv = new ModelAndView("listarInscricoes");
         //Iterable<Inscricao> inscricaos = inscricaoRepository.findInscricaoByIdUsuario(usuarioSite);
-        Iterable<Inscricao> inscricaos = inscricaoRepository.findInscricaoByIdUsuarioAndCheckinEquals(usuarioSite,0);
+        Iterable<Inscricao> inscricaos = inscricaoRepository.findInscricaoByIdUsuarioAndCheckinEquals(usuarioSite, 0);
         mv.addObject("inscricaos", inscricaos);
         return mv;
     }
 
     @RequestMapping("/listarInscricoesUsuarioComCheckin/{id}")
-    public ModelAndView listarInscricoesUsuarioComCheckin(@PathVariable("id") Integer id){
+    public ModelAndView listarInscricoesUsuarioComCheckin(@PathVariable("id") Integer id) {
 
         //SOMENTE com checkin = 1
         Usuario usuarioSite = new Usuario();
         usuarioSite.setId_usuario(id);
         ModelAndView mv = new ModelAndView("listarInscricoesComCheckIn");
-        Iterable<Inscricao> inscricaos = inscricaoRepository.findInscricaoByIdUsuarioAndCheckinEquals(usuarioSite,1);
+        Iterable<Inscricao> inscricaos = inscricaoRepository.findInscricaoByIdUsuarioAndCheckinEquals(usuarioSite, 1);
         mv.addObject("inscricaos", inscricaos);
 
         return mv;
     }
-
 
     @GetMapping
     public ResponseEntity<List<Inscricao>> getAll() {
@@ -79,21 +79,21 @@ public class InscricaoController {
     }
 
     @RequestMapping(value = "/cancelarInscricao/{idUsuario}/{idInscricao}", method = RequestMethod.GET)
-    public String cancelarInscricao(@PathVariable("idUsuario") Integer idUsuario,@PathVariable("idInscricao") Integer idInscricao) {
+    public String cancelarInscricao(@PathVariable("idUsuario") Integer idUsuario, @PathVariable("idInscricao") Integer idInscricao) {
 
-       Inscricao savedItem = inscricaoRepository.getById(idInscricao);
+        Inscricao savedItem = inscricaoRepository.getById(idInscricao);
+        enviarEmail("Usuario", "Sua Inscrição foi Cancelada");
 
-        if(calcularCancelamento(savedItem.getData())){
+        if (calcularCancelamento(savedItem.getData())) {
             inscricaoRepository.deleteById(idInscricao);
-            return "redirect:http://localhost:9090/usuario/homeUsuario/"+idUsuario;
-        }else{
+            return "redirect:http://localhost:9090/usuario/homeUsuario/" + idUsuario;
+        } else {
             return "errorCancel";
         }
-
     }
 
     @RequestMapping(value = "/fazerInscricao/{idUsuario}/{idEvento}", method = RequestMethod.GET)
-    public String fazerInscricao(@PathVariable("idUsuario") Integer idUsuario,@PathVariable("idEvento") Integer idEvento) {
+    public String fazerInscricao(@PathVariable("idUsuario") Integer idUsuario, @PathVariable("idEvento") Integer idEvento) {
 
         Inscricao savedItem = new Inscricao();
         Evento savedEvento = new Evento();
@@ -106,11 +106,13 @@ public class InscricaoController {
         savedItem.setCheckin(0);
         savedItem.setData(getDataDiaAtual());
         inscricaoRepository.save(savedItem);
-        return "redirect:http://localhost:9090/usuario/homeUsuario/"+idUsuario;
+        enviarEmail("Usuario", "Sua Inscrição foi Confirmada");
+
+        return "redirect:http://localhost:9090/usuario/homeUsuario/" + idUsuario;
     }
 
     @RequestMapping(value = "/fazerCheckin/{idUsuario}/{idEvento}", method = RequestMethod.GET)
-    public String fazerCheckin(@PathVariable("idUsuario") Integer idUsuario,@PathVariable("idEvento") Integer idEvento) {
+    public String fazerCheckin(@PathVariable("idUsuario") Integer idUsuario, @PathVariable("idEvento") Integer idEvento) {
 
         Inscricao savedItem = new Inscricao();
         Evento savedEvento = new Evento();
@@ -123,14 +125,15 @@ public class InscricaoController {
         savedItem.setData(getDataDiaAtual());
 
         //check para ver se existe, senão update
-        System.out.println((inscricaoRepository.countByIdUsuarioAndIdEvento(savedUsuario,savedEvento)).toString());
-        if(inscricaoRepository.countByIdUsuarioAndIdEvento(savedUsuario,savedEvento) == 0){
+        System.out.println((inscricaoRepository.countByIdUsuarioAndIdEvento(savedUsuario, savedEvento)).toString());
+        if (inscricaoRepository.countByIdUsuarioAndIdEvento(savedUsuario, savedEvento) == 0) {
             inscricaoRepository.save(savedItem);
             System.out.println("SALVOU NOVO");
-        }else{
-            inscricaoRepository.updateCheckIn(savedEvento,savedUsuario);
+        } else {
+            inscricaoRepository.updateCheckIn(savedEvento, savedUsuario);
             System.out.println("UPDATE");
         }
+        enviarEmail("Usuario", "Você fez Check-In no Evento");
 
         return "pgAcaoCompleta";
     }
@@ -146,35 +149,76 @@ public class InscricaoController {
         }
     }
 
+    @RequestMapping(value = "/certificado/{idUsuario}/{idEvento}", method = RequestMethod.GET)
+    public String requisitarCeritficado(@PathVariable("idUsuario") Integer idUsuario, @PathVariable("idEvento") Integer idEvento) {
 
 
+        Evento savedEvento = new Evento();
+        savedEvento.setId_evento(idEvento);
+        Usuario savedUsuario = new Usuario();
+        savedUsuario.setId_usuario(idUsuario);
+
+        String evento = savedEvento.getNome();
+        String usuario = savedUsuario.getNome();
+
+        enviarEmailCertificado(usuario, evento);
+
+        return "redirect:http://localhost:9090/usuario/homeUsuario/" + idUsuario;
+    }
 
 
-    // alterar depois
-    String getDataDiaAtual(){
+// alterar depois
+//----------------------####
+
+
+    String getDataDiaAtual() {
         Date dataAtual = new Date();
         String dataFormatada = new SimpleDateFormat("dd/MM/yyyy").format(dataAtual);
         return dataFormatada;
     }
 
-    boolean calcularCancelamento(String dataInsc){
-        DateFormat df = new SimpleDateFormat ("dd/MM/yyyy");
+    boolean calcularCancelamento(String dataInsc) {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         df.setLenient(false);
         Date d1 = null;
         Date d2 = null;
         try {
-            d1 = df.parse (dataInsc);
-            d2 = df.parse (getDataDiaAtual());
-        } catch (java.text.ParseException evt ) {}
+            d1 = df.parse(dataInsc);
+            d2 = df.parse(getDataDiaAtual());
+        } catch (java.text.ParseException evt) {
+        }
         long dt = (d2.getTime() - d1.getTime()) + 3600000;
         long dias = (dt / 86400000L);
 
         System.out.println(dias);
-        if ( dt / 86400000L <= 2){
+        if (dt / 86400000L <= 2) {
             return true;
-        }else{
+        } else {
             return false;
         }
+    }
+
+    //http://localhost:9090/email/send/
+    private String enviarEmail(String nome, String msg) {
+
+        String nomeUsuario = nome;
+        String mensagem = msg;
+
+        String uri = "http://localhost:9090/email/send/" + nome + "/" + msg;
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
+    }
+
+    private String enviarEmailCertificado(String nome,String evento) {
+
+        String nomeUsuario = nome;
+        String msg = "Olá, este é o certificado de participação no evento "+evento;
+
+        String uri = "http://localhost:9090/email/send/" + nome + "/" + msg;
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+        return result;
     }
 
 }
